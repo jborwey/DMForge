eec18e9811f0ad57688661df6b534f7a5678d5bd
Joshua Borwey <joshua.borwey@gmail.com>
2023-04-22 13:33:51 -0500

    removed unused file folder

--- ClientApp/src/components/NpcGenerateForm.jsx
+++ ClientApp/src/components/NpcGenerateForm.jsx
@@ -14,7 +14,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import InfoWidget from './info/InfoWidget';
+import InfoWidget from './InfoWidget';
 
 const NpcGenerateForm = ({ }) => {
     const theme = useTheme();
