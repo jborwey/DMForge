be823f6752431c34f69c208ee5711d13dae96ee3
Jeff Borwey <jeffborwey@gmail.com>
2023-04-24 13:23:07 -0700

    Adding chip text editor

--- ClientApp/src/scenes/items/index.jsx
+++ ClientApp/src/scenes/items/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "components/Header";
 import { Box, Card, CardActions, CardContent, Collapse, Button, Typography, Rating, useTheme, useMediaQuery } from "@mui/material";
 import EntityTextBox from "../../components/EntityTextBox";
+import ChipTextField from "../../components/ChipTextField";
 
 const Items = () => {
     return(
@@ -9,6 +10,9 @@ const Items = () => {
       sx={{paddingLeft:"2rem"}}>
         <Header title="ITEMS" subtitle="See your party's list of items." />
         <EntityTextBox />
+        <div>
+          <ChipTextField />
+        </div>
       </Box>
     );
 };
